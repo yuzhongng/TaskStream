b029a7c3f4b5ccce161d4d2fd79e97ae2f0d0212
yuzhongng <yuzhongng@gmail.com>
2022-02-18 17:20:42 +0800

    Add count deadline with stream method

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -32,6 +32,13 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesWithStream(ArrayList<Task> tasks) {
+        int count = 0;
+        count = (int) tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count;
+    }
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
